e316712d01b5caf7d3265d3d273bd75fe0ce98ff
devcode <devcode@unipromax.pl>
2021-07-19 20:38:54 +0200

    20210719

---
+++ SODP.UI/ViewModels/DesignersListVM.cs
@@ -0,0 +1,12 @@
+﻿using SODP.Shared.DTO;
+using SODP.Shared.Response;
+using System.Collections.Generic;
+
+namespace SODP.UI.ViewModels
+{
+    public class DesignersListVM
+    {
+        public IList<DesignerDTO> Designers { get; set; }
+        public PageInfo PageInfo { get; set; }
+    }
+}
