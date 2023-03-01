a6c78b5a16b368387e5c8530f5cbcd19daffcdc9
devcode <devcode@unipromax.pl>
2023-03-01 06:11:26 +0100

    20230228

---
+++ SODP.Application/Exceptions/AppException.cs
@@ -0,0 +1,14 @@
+﻿using SODP.Domain.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SODP.Application.Exceptions
+{
+    public abstract class AppException : Exception
+    {
+        protected AppException(string message) : base(message) { }
+    }
+}
